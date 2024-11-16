b9a732e4916849a060bb65ba04c184643522a0ae
NotWeButRun <onion.dedede@gmail.com>
2024-11-16 10:47:14 +0000

    [fix-ed] もとのコードに含まれていた実行を妨げる（軽微な）バグを修正

--- emecom_gen/model/game/__init__.py
+++ emecom_gen/model/game/__init__.py
@@ -1,7 +1,7 @@
 from .game_output import GameOutput
 from .game_base import GameBase
 from .ensemble_beta_vae_game import EnsembleBetaVAEGame
-from .ensemble_em_game import EnsembleEMGame
+# from .ensemble_em_game import EnsembleEMGame
 from .baseline import InputDependentBaseline
 from .beta_scheduler import (
     ConstantBetaScheduler,
