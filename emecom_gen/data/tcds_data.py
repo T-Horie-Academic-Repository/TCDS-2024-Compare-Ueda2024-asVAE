d6775042b499ffdae60722589de941e37ec5b52e
NotWeButRun <onion.dedede@gmail.com>
2024-12-05 07:26:20 +0000

    [featur-ed] 外部から感覚情報カテゴリを読み込めるようにした

--- emecom_gen/data/tcds_data.py
+++ emecom_gen/data/tcds_data.py
@@ -1,11 +1,29 @@
 """ IEEE TCDS 2024 の比較実験に使うためのモジュール """
 
-TRAIN_DATA = [
-    [0, 0, 0, 0],
-    [1, 2, 1, 2],
-]
+import pandas as pd
 
-TEST_DATA = [
-    [0, 1, 0, 1],
-    [1, 3, 1, 3],
-]
+# TRAIN_DATA = [
+#     [0, 0, 0, 0],
+#     [1, 2, 1, 2],
+#     [0, 0, 0, 0],
+#     [1, 2, 1, 2],
+# ]
+TRAIN_DATA = pd.read_csv("../datas/train_zs_30.csv").values.tolist()
+TRAIN_DATA.extend(pd.read_csv("../datas/train_extended_zs_70.csv").values.tolist())
+
+
+# TEST_DATA = [
+#     [0, 1, 0, 1],
+#     [1, 3, 1, 3],
+#     [0, 0, 0, 0],
+#     [1, 2, 1, 2],
+#     [0, 0, 0, 0],
+#     [1, 2, 1, 2],
+# ]
+TEST_DATA = pd.read_csv("../datas/test_zs_10.csv").values.tolist()
+
+print(TRAIN_DATA)
+print(TEST_DATA)
+
+assert len(TRAIN_DATA) == 100 ## 仮コード
+assert len(TEST_DATA) == 10 ## 仮コード
